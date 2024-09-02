e42e7db3ebf1afb333dc1986e97b762f27002ffc
ruoflhfd4 <137387786+ruoflhfd4@users.noreply.github.com>
2024-09-02 10:19:31 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("8bdeb30d513f1c2412f04b90920e656b7716dcacead5b9732965b32e1d4b3014","t"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
